f7cb63b55114b554f2304b3a1c40c90020d30349
codingSlate <ananta2021k@gmail.com>
2021-12-07 21:39:52 +0530

    useRef

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # react-1wtvva
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1wtvva)
+
+
+## useRef
+- its an object constins current property
+- view never updated as compare to useState
--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Stopwatch = () => {
   const [time, setTime] = useState(0);
+
+  const curTime = useRef(0);
+
   useEffect(() => {
     const myTimer = setInterval(() => {
-      setTime(time + 1);
+      curTime.current++;
+      // setTime(time + 1);
+      setTime(curTime.current);
       // console.log('Its run because of closure: Timer set ');
       // console.log(time); // but the time is 0
     }, 1000);
